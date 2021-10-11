4f16da08d53c451b9e93d4c17e3f325eeb459c11
Kishan <saxenakishan97@gmail.com>
2021-10-12 02:01:08 +0530

    Dashboard Complete

--- routes/dashboard_route.js
+++ routes/dashboard_route.js
@@ -1,10 +1,24 @@
 const express=require('express');
 const router=express.Router();
 
+const quiz=require('../models/quiz_schema');
+
 const {ensureAuth}=require('../utils/auth');
 
-router.get('/dashboard',ensureAuth,(req,res)=>{
-    res.render("dashboard",{layout:"main"});
+router.get('/dashboard',ensureAuth,async(req,res)=>{
+    
+    try{
+        const quizzes=await quiz.find({},{"_id":1,"quiz_name":1});
+        console.log(quizzes);
+        res.render("dashboard",{layout:"main",quizzes:JSON.stringify(quizzes)});
+        
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+
 });
+
 
 module.exports=router;
