efc0aea280588e5fa808837020285f28be71b337
Kishan <saxenakishan97@gmail.com>
2021-10-13 02:56:11 +0530

    Quiz Submission Functionality Added

--- routes/dashboard_route.js
+++ routes/dashboard_route.js
@@ -19,21 +19,33 @@ router.get('/dashboard',ensureAuth,async(req,res)=>{
 
 });
 
-router.get('/attempt-quiz/:id',async (req,res)=>{
+router.get('/attempt-quiz/:id',ensureAuth,async (req,res)=>{
    
     try{
         const cur_quiz=await quiz.findOne({"_id":req.params.id},{"quiz_name":1,"questions":1,"_id":0});
-        console.log(cur_quiz);
-        res.render("quiz_attempt",{layout:"main",quiz_data:cur_quiz});
+        res.render("quiz_attempt",{layout:"main",quiz_data:JSON.stringify(cur_quiz)});
     }
     catch(e)
     {
         console.log(e);
         res.render("Error Occured");
-
     }
 
 });
+
+router.post('/attempt-quiz/:id',ensureAuth,async (req,res)=>{
+  
+    try{
+        const cur_quiz=await quiz.findOne({"_id":req.params.id},{"_id":0,"quiz_name":0});
+        console.log(cur_quiz);
+
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+    res.send("Ans Submitted");
+})
 
 
 module.exports=router;
