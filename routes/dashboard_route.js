00ed394bacd4f08a0a0f4d9ff2d10861b9d06f56
Kishan <saxenakishan97@gmail.com>
2021-10-14 03:22:14 +0530

    Bug Fixes

--- routes/dashboard_route.js
+++ routes/dashboard_route.js
@@ -1,67 +1,98 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-const quiz=require('../models/quiz_schema');
+const user = require('../models/user_schema');
+const quiz = require('../models/quiz_schema');
 
-const {ensureAuth}=require('../utils/auth');
+const { ensureAuth } = require('../utils/auth');
 
-router.get('/dashboard',ensureAuth,async(req,res)=>{
-    
-    try{
-        const quizzes=await quiz.find({},{"_id":1,"quiz_name":1});
-        res.render("dashboard",{layout:"main",quizzes:JSON.stringify(quizzes)});   
+router.get('/dashboard', ensureAuth, async (req, res) => {
+
+    try {
+        const quizzes = await quiz.find({}, { "_id": 1, "quiz_name": 1,"timeStamp":1});
+        res.render("dashboard", { layout: "main", quizzes: JSON.stringify(quizzes) });
     }
-    catch(e)
-    {
+    catch (e) {
         console.log(e);
         res.render("Error Occured");
     }
 
 });
 
-router.get('/attempt-quiz/:id',ensureAuth,async (req,res)=>{
-   
-    try{
-        const cur_quiz=await quiz.findOne({"_id":req.params.id},{"quiz_name":1,"questions":1,"_id":0});
-        res.render("quiz_attempt",{layout:"main",quiz_data:JSON.stringify(cur_quiz)});
+router.get('/attempt-quiz/:id', ensureAuth, async (req, res) => {
+
+    try {
+        const cur_quiz = await quiz.findOne({ "_id": req.params.id }, { "quiz_name": 1, "questions": 1, "_id": 0 });
+        res.render("quiz_attempt", { layout: "main", quiz_data: JSON.stringify(cur_quiz) });
     }
-    catch(e)
-    {
+    catch (e) {
         console.log(e);
         res.render("Error Occured");
     }
 
 });
 
-router.post('/attempt-quiz/:id',ensureAuth,async (req,res)=>{
-  
-    try{
-        const cur_quiz=await quiz.findOne({"_id":req.params.id},{"_id":0});
-        let quiz_questions=cur_quiz.questions;
+router.post('/attempt-quiz/:id', ensureAuth, async (req, res) => {
 
-        let counter=1;
-        let score=0;
-        quiz_questions.forEach(question=>{
-            if(`q${counter}` in req.body)
-            {
-                let option=req.body[`q${counter}`];
-                if(question[`option_${option[1]}`]==question.ans)
-                score++;
+    try {
+        const cur_quiz = await quiz.findOne({ "_id": req.params.id }, { "_id": 0 });
+        let quiz_questions = cur_quiz.questions;
+
+        let counter = 1;
+        let score = 0;
+        quiz_questions.forEach(question => {
+            if (`q${counter}` in req.body) {
+                let option = req.body[`q${counter}`];
+                if (question[`option_${option[1]}`] == question.ans)
+                    score++;
                 counter++;
             }
         });
-        let quiz_link=`/attempt-quiz/${req.params.id}`;
-        score=((score/(counter-1))*100).toPrecision(4);
-        res.render("viewscore",{score:score,quiz_name:cur_quiz.quiz_name,quiz_link});
+        score = ((score / (counter - 1)) * 100).toPrecision(4);
+        score = String(score);
+        const dat = await user.findOneAndUpdate({ "_id": req.user._id }, {
+            $push: {
+                quizzes: {
+                    quiz_id: req.params.id,
+                    score: score
+                }
+            }
+        });
 
+        console.log(dat);
+        res.redirect(`/viewscore/${score}/${cur_quiz.quiz_name}/${req.params.id}`);
     }
-    catch(e)
-    {
+    catch (e) {
         console.log(e);
         res.render("Error Occured");
-
     }
-})
+});
 
+router.get('/viewscore/:score/:quiz_name/:id', (req, res) => {
+    res.render('viewscore', { score: req.params.score, quiz_name: req.params.quiz_name, quiz_id: req.params.id });
+});
 
-module.exports=router;
+router.get('/myscores', ensureAuth, async (req, res) => {
+
+    try {
+        console.log(1);
+        const scores = await user.findOne({ '_id': req.user._id });
+        var quizs = [];
+
+        for(let lquiz of scores.quizzes)
+        {
+            let cur_quiz = await quiz.findOne({ "_id": lquiz.quiz_id });
+            console.log(lquiz);
+            quizs.push({ "quiz_name": cur_quiz.quiz_name, "score": lquiz.score ,"time":lquiz.timeStamp});
+        };
+
+        console.log(quizs);
+
+        res.render("myscores",{quiz_data:quizs});
+    }
+    catch (e) {
+        console.log(e);
+        res.render("Error Occured")
+    }
+});
+module.exports = router;
